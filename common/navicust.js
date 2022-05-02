2d2161933760015d14b86ee94d2860641afed9e5
bigfarts <bigfarts@punymail.com>
2022-05-02 00:31:15 -0700

    update navicust styling

--- common/navicust.js
+++ common/navicust.js
@@ -2,7 +2,7 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
     const borderColor = "#29314a";
     const cellColor = "#105284";
 
-    const borderWidth = 6;
+    const borderWidth = 4;
 
     const NAVICUST_COLORS = {
         red: {
@@ -94,10 +94,14 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
         const noBugColorsTable = document.createElement("table");
         colorsContainer.appendChild(noBugColorsTable);
 
-        noBugColorsTable.style.boxSizing = "border-box";
-        noBugColorsTable.style.borderCollapse = "separate";
-        noBugColorsTable.style.borderSpacing = "4px";
+        noBugColorsTable.style.display = "inline-block";
         noBugColorsTable.style.background = borderColor;
+        noBugColorsTable.style.boxSizing = "border-box";
+        noBugColorsTable.style.borderStyle = "solid";
+        noBugColorsTable.style.borderColor = borderColor;
+        noBugColorsTable.style.borderWidth = `${borderWidth / 4}px`;
+        noBugColorsTable.style.borderSpacing = 0;
+        noBugColorsTable.style.borderCollapse = "separate";
 
         const noBugColorsTr = document.createElement("tr");
         noBugColorsTable.appendChild(noBugColorsTr);
@@ -106,10 +110,20 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
             const colorTd = document.createElement("td");
             noBugColorsTr.appendChild(colorTd);
 
-            colorTd.style.height = `${borderWidth * 4}px`;
-            colorTd.style.width = `${borderWidth * 6}px`;
+            colorTd.style.borderStyle = "solid";
+            colorTd.style.borderColor = borderColor;
             colorTd.style.boxSizing = "border-box";
-            colorTd.style.background =
+            colorTd.style.borderWidth = `${borderWidth / 2}px`;
+            colorTd.style.width = `${borderWidth * 8}px`;
+            colorTd.style.height = `${borderWidth * 5}px`;
+            colorTd.style.padding = 0;
+
+            const colorDiv = document.createElement("div");
+            colorTd.appendChild(colorDiv);
+
+            colorDiv.style.width = "100%";
+            colorDiv.style.height = "100%";
+            colorDiv.style.background =
                 i < colors.length
                     ? NAVICUST_COLORS[colors[i]].plusColor
                     : cellColor;
@@ -117,9 +131,13 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
 
         const bugColorsTable = document.createElement("table");
         colorsContainer.appendChild(bugColorsTable);
+        bugColorsTable.style.display = "inline-block";
+        bugColorsTable.style.borderStyle = "solid";
+        bugColorsTable.style.borderColor = "transparent";
         bugColorsTable.style.boxSizing = "border-box";
+        bugColorsTable.style.borderWidth = `${borderWidth / 4}px`;
+        bugColorsTable.style.borderSpacing = 0;
         bugColorsTable.style.borderCollapse = "separate";
-        bugColorsTable.style.borderSpacing = "4px";
 
         const bugColorsTr = document.createElement("tr");
         bugColorsTable.appendChild(bugColorsTr);
@@ -130,32 +148,38 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
             const colorTd = document.createElement("td");
             bugColorsTr.appendChild(colorTd);
 
-            colorTd.style.height = `${borderWidth * 4}px`;
-            colorTd.style.width = `${borderWidth * 6}px`;
+            colorTd.style.borderStyle = "solid";
+            colorTd.style.borderColor = "transparent";
             colorTd.style.boxSizing = "border-box";
-            colorTd.style.background = NAVICUST_COLORS[color].plusColor;
+            colorTd.style.borderWidth = `${borderWidth / 2}px`;
+            colorTd.style.width = `${borderWidth * 8}px`;
+            colorTd.style.height = `${borderWidth * 5}px`;
+            colorTd.style.padding = 0;
+
+            const colorDiv = document.createElement("div");
+            colorTd.appendChild(colorDiv);
+
+            colorDiv.style.width = "100%";
+            colorDiv.style.height = "100%";
+            colorDiv.style.background = NAVICUST_COLORS[color].plusColor;
         }
 
         const gridContainer = document.createElement("div");
         container.appendChild(gridContainer);
 
         gridContainer.style.position = "relative";
-        gridContainer.style.width = `${
-            borderWidth * (packing.ncols * 10 + 1)
-        }px`;
-        gridContainer.style.height = `${
-            borderWidth * (packing.nrows * 10 + 1)
-        }px`;
 
         const table = document.createElement("table");
         gridContainer.appendChild(table);
 
-        table.style.boxSizing = "border-box";
-        table.style.borderCollapse = "separate";
-        table.style.borderSpacing = `${borderWidth}px`;
         table.style.background = borderColor;
-        table.style.width = `${borderWidth * (packing.ncols * 10 + 1)}px`;
-        table.style.height = `${borderWidth * (packing.nrows * 10 + 1)}px`;
+        table.style.borderStyle = "solid";
+        table.style.borderColor = borderColor;
+        table.style.borderWidth = `${borderWidth / 2}px`;
+        table.style.boxSizing = "border-box";
+        table.style.borderSpacing = 0;
+        table.style.borderCollapse = "separate";
+        table.style.borderRadius = "4px";
 
         for (let i = 0; i < packing.nrows; ++i) {
             const row = document.createElement("tr");
@@ -166,26 +190,88 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
                 row.appendChild(cell);
 
                 const idx = packing[i * packing.ncols + j];
-                cell.className = "navicust-cell";
-                cell.style.boxSizing = "border-box";
-                cell.style.background = cellColor;
-                cell.style.textAlign = "center";
-                cell.style.verticalAlign = "middle";
-                cell.style.fontSize = `${borderWidth * 3.5}px`;
-                cell.style.fontFamily = "sans-serif";
-                cell.style.fontWeight = "bold";
-                cell.style.position = "relative";
-                cell.style.width = `${borderWidth * 9}px`;
-                cell.style.height = `${borderWidth * 9}px`;
+
+                const softBorders = [];
+                const hardBorders = [];
 
                 if (
+                    idx == -1 ||
+                    j <= 0 ||
+                    packing[i * packing.ncols + (j - 1)] != idx
+                ) {
+                    hardBorders.push(
+                        `inset ${borderWidth / 2}px 0 ${borderColor}`
+                    );
+                } else {
+                    softBorders.push(
+                        `inset ${borderWidth / 2}px 0 ${
+                            NAVICUST_COLORS[infos[idx].color].plusColor
+                        }`
+                    );
+                }
+
+                if (
+                    idx == -1 ||
+                    j >= packing.ncols - 1 ||
+                    packing[i * packing.ncols + (j + 1)] != idx
+                ) {
+                    hardBorders.push(
+                        `inset ${-borderWidth / 2}px 0 ${borderColor}`
+                    );
+                } else {
+                    softBorders.push(
+                        `inset ${-borderWidth / 2}px 0 ${
+                            NAVICUST_COLORS[infos[idx].color].plusColor
+                        }`
+                    );
+                }
+
+                if (
+                    idx == -1 ||
+                    i <= 0 ||
+                    packing[(i - 1) * packing.ncols + j] != idx
+                ) {
+                    hardBorders.push(
+                        `inset 0 ${borderWidth / 2}px ${borderColor}`
+                    );
+                } else {
+                    softBorders.push(
+                        `inset 0 ${borderWidth / 2}px ${
+                            NAVICUST_COLORS[infos[idx].color].plusColor
+                        }`
+                    );
+                }
+
+                if (
+                    idx == -1 ||
+                    i >= packing.nrows - 1 ||
+                    packing[(i + 1) * packing.ncols + j] != idx
+                ) {
+                    hardBorders.push(
+                        `inset 0 ${-borderWidth / 2}px ${borderColor}`
+                    );
+                } else {
+                    softBorders.push(
+                        `inset 0 ${-borderWidth / 2}px ${
+                            NAVICUST_COLORS[infos[idx].color].plusColor
+                        }`
+                    );
+                }
+
+                cell.style.width = `${borderWidth * 9}px`;
+                cell.style.height = `${borderWidth * 9}px`;
+                cell.style.padding = 0;
+                cell.style.background = cellColor;
+                cell.style.opacity =
                     i < safeAreaTop ||
                     i >= safeAreaTop + safeAreaNrows ||
                     j < safeAreaLeft ||
                     j >= safeAreaLeft + safeAreaNcols
-                ) {
-                    cell.style.opacity = 0.25;
-                }
+                        ? 0.25
+                        : 1.0;
+                cell.style.fontSize = `${borderWidth * 3.5}px`;
+                cell.style.fontFamily = "sans-serif";
+                cell.style.fontWeight = "bold";
 
                 if (
                     deadCorners &&
@@ -197,13 +283,26 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
                     cell.style.background = borderColor;
                 }
 
+                const inner = document.createElement("div");
+                cell.appendChild(inner);
+
+                inner.style.width = "100%";
+                inner.style.height = "100%";
+                inner.style.boxShadow = [...hardBorders, ...softBorders].join(
+                    ","
+                );
+                inner.style.display = "flex";
+                inner.style.alignItems = "center";
+                inner.style.justifyContent = "center";
+                inner.className = "navicust-cell";
+
                 if (idx != -1) {
                     const info = infos[idx];
                     if (info.isSolid) {
-                        cell.style.background =
+                        inner.style.background =
                             NAVICUST_COLORS[info.color].color;
                     } else {
-                        cell.style.background = `conic-gradient(
+                        inner.style.background = `conic-gradient(
                             from 90deg at ${borderWidth}px ${borderWidth}px,
                             ${NAVICUST_COLORS[info.color].color} 90deg,
                             ${NAVICUST_COLORS[info.color].plusColor} 0
@@ -211,28 +310,48 @@ const { renderNavicust, clearNavicustNumbers, NAVICUST_COLORS } = (() => {
                         calc(100% + ${borderWidth}px / 2) calc(100% + ${borderWidth}px / 2) /
                         calc(50% + ${borderWidth}px) calc(50% + ${borderWidth}px)`;
                     }
-                    cell.textContent = idx + 1;
+                    inner.textContent = idx + 1;
                 }
             }
         }
 
-        const commandLineTop = document.createElement("div");
+        const commandLineTop = document.createElement("hr");
         gridContainer.appendChild(commandLineTop);
-        commandLineTop.style.position = "absolute";
-        commandLineTop.style.width = table.style.width;
-        commandLineTop.style.height = `${borderWidth}px`;
-        commandLineTop.style.top = `${borderWidth * (commandLine * 10 + 2)}px`;
-        commandLineTop.style.background = borderColor;
-
-        const commandLineBottom = document.createElement("div");
-        gridContainer.appendChild(commandLineBottom);
-        commandLineBottom.style.position = "absolute";
-        commandLineBottom.style.width = table.style.width;
-        commandLineBottom.style.height = `${borderWidth}px`;
-        commandLineBottom.style.top = `${
-            borderWidth * ((commandLine + 1) * 10 - 2)
+        commandLineTop.style.top = `${
+            borderWidth * commandLine * 9 + borderWidth * 2.5
         }px`;
-        commandLineBottom.style.background = borderColor;
+        commandLineTop.style.margin = 0;
+        commandLineTop.style.padding = 0;
+        commandLineTop.style.position = "absolute";
+        commandLineTop.style.width = "100%";
+        commandLineTop.style.borderColor = borderColor;
+        commandLineTop.style.borderLeftStyle = "none";
+        commandLineTop.style.borderRightStyle = "none";
+        commandLineTop.style.borderTopStyle = "none";
+        commandLineTop.style.borderBottomStyle = "solid";
+        commandLineTop.style.boxSizing = "border-box";
+        commandLineTop.style.borderWidth = `${borderWidth}px`;
+        commandLineTop.style.pointerEvents = "none";
+        commandLineTop.style.opacity = "1.0";
+
+        const commandLineBottom = document.createElement("hr");
+        gridContainer.appendChild(commandLineBottom);
+        commandLineBottom.style.top = `${
+            borderWidth * commandLine * 9 + borderWidth * 7 - borderWidth / 2
+        }px`;
+        commandLineBottom.style.margin = 0;
+        commandLineBottom.style.padding = 0;
+        commandLineBottom.style.position = "absolute";
+        commandLineBottom.style.width = "100%";
+        commandLineBottom.style.borderColor = borderColor;
+        commandLineBottom.style.borderLeftStyle = "none";
+        commandLineBottom.style.borderRightStyle = "none";
+        commandLineBottom.style.borderTopStyle = "solid";
+        commandLineBottom.style.borderBottomStyle = "none";
+        commandLineBottom.style.boxSizing = "border-box";
+        commandLineBottom.style.borderWidth = `${borderWidth}px`;
+        commandLineBottom.style.pointerEvents = "none";
+        commandLineBottom.style.opacity = "1.0";
 
         return container;
     }
